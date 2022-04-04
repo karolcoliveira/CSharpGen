a919dfc2ee1e80c44ad3aa278747decc6e4614b3
Karol Oliveira <oliveiraakarol@hotmail.com>
2022-04-04 19:23:47 -0300

    Exercicios CSharp

---
+++ Exercicio2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float peso, altura, IMC;
+
+            Console.WriteLine("Insira seu peso: ");
+            peso = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira sua altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            IMC = peso / (altura * altura);
+
+            Console.WriteLine("Seu IMC é: " + IMC);
+
+
+        }
+    }
+}
