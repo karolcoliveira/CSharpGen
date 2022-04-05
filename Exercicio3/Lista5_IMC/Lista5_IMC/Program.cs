d1c28756d0b4f7ed670678814e2edbeb8a0e6285
Karol Oliveira <oliveiraakarol@hotmail.com>
2022-04-05 15:40:17 -0300

    @karolcoliveira [commit 05/04/22 Lista 5 C#]

---
+++ Exercicio3/Lista5_IMC/Lista5_IMC/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lista5_IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float peso, altura, IMC;
+
+            Console.WriteLine("Insira seu peso: ");
+            peso = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira sua altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            IMC = peso / (altura * altura);
+
+            Console.WriteLine("Seu IMC é: " + IMC);
+
+            if (IMC < 18.5)
+            {
+             Console.WriteLine("Abaixo do peso");
+            }
+
+            else if (IMC < 25)
+            {
+                Console.WriteLine("Peso normal");
+            }
+
+            else if (IMC < 30)
+            {
+                Console.WriteLine("Sobrepeso");
+            }
+
+            else
+            {
+                Console.WriteLine("Obesidade");
+            }
+        }
+    }
+}
