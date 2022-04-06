5bf18c3c3c7a33720f2dbe70bdf05cc73dc268f3
Karol Oliveira <oliveiraakarol@hotmail.com>
2022-04-06 15:22:23 -0300

    @karolcoliveira [commit 06/04/2022 Lista 6 C#]

---
+++ Exercicio4/Lista6_TabuadaMalu/Lista6_TabuadaMalu/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lista6_TabuadaMalu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int numero;
+            Console.WriteLine("Tabuada da Malu");
+            Console.WriteLine("Insira o número que você deseja multiplicar: ");
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int A = 0; A <= 10; A++)
+            {
+             int resultado = (numero * A);
+
+             Console.WriteLine($"{numero} x {A} = {resultado}");
+            }
+        }
+        
+    }
+}
